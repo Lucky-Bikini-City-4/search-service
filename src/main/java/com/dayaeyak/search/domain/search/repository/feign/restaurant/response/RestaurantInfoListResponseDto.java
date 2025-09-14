b65b69df89cb57aaf03198b6a7f43bbc054d7b52
dayaelee <dayae.dev@gmail.com>
2025-09-15 03:47:36 +0900

    feat: 음식점 관련 dto 추가

--- src/main/java/com/dayaeyak/search/domain/search/repository/feign/restaurant/response/RestaurantInfoListResponseDto.java
+++ src/main/java/com/dayaeyak/search/domain/search/repository/feign/restaurant/response/RestaurantInfoListResponseDto.java
@@ -1,7 +1,16 @@
 package com.dayaeyak.search.domain.search.repository.feign.restaurant.response;
 
+import lombok.Getter;
+
 import java.util.List;
 
+
+@Getter
 public class RestaurantInfoListResponseDto {
-    List<RestaurantInfoResponseDto> content;
+    private List<RestaurantInfoResponseDto> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
 }
