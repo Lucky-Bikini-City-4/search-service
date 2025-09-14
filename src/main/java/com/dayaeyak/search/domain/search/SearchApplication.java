b9c40153b1807e72aa1c4c4a4ea1126748a667ea
dayaelee <dayae.dev@gmail.com>
2025-09-15 03:35:10 +0900

    chore: feignClient 사용을 위한 어노테이션 추가

--- src/main/java/com/dayaeyak/search/domain/search/SearchApplication.java
+++ src/main/java/com/dayaeyak/search/domain/search/SearchApplication.java
@@ -1,8 +1,10 @@
-package com.dayaeyak.search;
+package com.dayaeyak.search.domain.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SearchApplication {
 
