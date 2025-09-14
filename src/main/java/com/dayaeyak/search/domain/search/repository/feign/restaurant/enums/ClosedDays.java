a9a1bb4db157ac092c7f2028e30e733268bde7b8
dayaelee <dayae.dev@gmail.com>
2025-09-15 03:27:55 +0900

    feat: 음식점 관련 ENUM 타입 추가

---
+++ src/main/java/com/dayaeyak/search/domain/search/repository/feign/restaurant/enums/ClosedDays.java
@@ -0,0 +1,26 @@
+package com.dayaeyak.search.domain.search.repository.feign.restaurant.enums;
+
+import java.time.DayOfWeek;
+
+public enum ClosedDays {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    NONE; // 매일 영업
+
+//    public static ClosedDays fromDayOfWeek(DayOfWeek dow) {
+//        return switch (dow) {
+//            case MONDAY -> MONDAY;
+//            case TUESDAY -> TUESDAY;
+//            case WEDNESDAY -> WEDNESDAY;
+//            case THURSDAY -> THURSDAY;
+//            case FRIDAY -> FRIDAY;
+//            case SATURDAY -> SATURDAY;
+//            case SUNDAY -> SUNDAY;
+//        };
+//    }
+}
---
+++ src/main/java/com/dayaeyak/search/domain/search/repository/feign/restaurant/enums/WaitingStatus.java
@@ -0,0 +1,6 @@
+package com.dayaeyak.search.domain.search.repository.feign.restaurant.enums;
+
+public enum WaitingStatus {
+    ON,
+    OFF
+}
