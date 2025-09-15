4b375104eb15e9b237313f6eda784aa162ff5005
dayaelee <dayae.dev@gmail.com>
2025-09-16 01:47:29 +0900

    feat: 통합 조회를 위한 비즈니스 로직 추가

--- src/main/java/com/dayaeyak/search/domain/search/service/SimpleQueryService.java
+++ src/main/java/com/dayaeyak/search/domain/search/service/SimpleQueryService.java
@@ -5,17 +5,24 @@ import com.dayaeyak.search.domain.search.repository.feign.exhibition.enums.Regio
 import com.dayaeyak.search.domain.search.repository.feign.exhibition.enums.SearchType;
 import com.dayaeyak.search.domain.search.repository.feign.exhibition.response.ExhibitionFinalListResponseDto;
 import com.dayaeyak.search.domain.search.repository.feign.exhibition.response.ExhibitionFinalResponseDto;
+import com.dayaeyak.search.domain.search.repository.feign.exhibition.response.ExhibitionInfoResponseDto;
 import com.dayaeyak.search.domain.search.repository.feign.performance.PerformanceFeignClient;
 import com.dayaeyak.search.domain.search.repository.feign.performance.enums.Grade;
 import com.dayaeyak.search.domain.search.repository.feign.performance.response.PerformanceFinalListResponseDto;
 import com.dayaeyak.search.domain.search.repository.feign.performance.response.PerformanceFinalResponseDto;
+import com.dayaeyak.search.domain.search.repository.feign.performance.response.PerformanceInfoResponseDto;
 import com.dayaeyak.search.domain.search.repository.feign.restaurant.RestaurantFeignClient;
 import com.dayaeyak.search.domain.search.repository.feign.restaurant.response.RestaurantFinalListResponseDto;
 import com.dayaeyak.search.domain.search.repository.feign.restaurant.response.RestaurantFinalResponseDto;
+import com.dayaeyak.search.domain.search.repository.feign.restaurant.response.RestaurantInfoResponseDto;
+import com.dayaeyak.search.domain.search.repository.response.AllFinalResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -82,10 +89,79 @@ public class SimpleQueryService {
     }
 
     // 음식점 목록 조회
-    public RestaurantFinalListResponseDto getRestaurants(Long userId, String role){
+    public RestaurantFinalListResponseDto getRestaurants(Long userId, String role, String type){
+        var dto = restaurantFeignClient.findRestaurantInfoList(userId, role);
+        System.out.println(type);
+        var cmp = Comparator.comparing(
+                RestaurantInfoResponseDto::openTime,
+                Comparator.nullsLast(Comparator.naturalOrder())
+        );
+        if ("desc".equalsIgnoreCase(type)) {
+            cmp = cmp.reversed();
+            dto.getData().getContent().sort(cmp);
+        }
+        if ("asc".equalsIgnoreCase(type)) {
+            dto.getData().getContent().sort(cmp);
+        }
+        return dto;
+    }
 
-        RestaurantFinalListResponseDto restaurants =
-                restaurantFeignClient.findRestaurantInfoList(userId, role);
-        return restaurants;
+
+
+
+
+    // 전체 목록 조회
+    public List<AllFinalResponseDto> getAlls(Long userId, String role, String type,
+                                       int page,
+                                       int size,
+                                       Region region,
+                                       Grade grade,
+                                       LocalDate startDate,
+                                       LocalDate endDate,
+                                       String keyword,
+                                       SearchType searchType
+                                           ){
+
+        var dtoRestaurant = restaurantFeignClient.findRestaurantInfoList(userId, role);
+
+        var dtoExhibitions =
+                exhibitionFeignClient.findExhibitionInfoList(
+                        page,
+                        size,
+                        region,
+                        grade,
+                        startDate,
+                        endDate,
+                        keyword,
+                        searchType);
+        var dtoPerformances =
+                performanceFeignClient.findPerformanceInfoList();
+
+        List<AllFinalResponseDto> dtoRawPerformance = new ArrayList<>();
+        for(PerformanceInfoResponseDto x: dtoPerformances.getData().getPerformances()) {
+            dtoRawPerformance.add(AllFinalResponseDto.fromPerformance(x));
+        }
+        for(ExhibitionInfoResponseDto x: dtoExhibitions.getData()) {
+            dtoRawPerformance.add(AllFinalResponseDto.fromExhibition(x));
+        }
+        for(RestaurantInfoResponseDto x: dtoRestaurant.getData().getContent()) {
+            dtoRawPerformance.add(AllFinalResponseDto.fromRestaurant(x));
+        }
+        return dtoRawPerformance;
+
+//        var dto = restaurantFeignClient.findRestaurantInfoList(userId, role);
+//        System.out.println(type);
+//        var cmp = Comparator.comparing(
+//                RestaurantInfoResponseDto::getOpenTime,
+//                Comparator.nullsLast(Comparator.naturalOrder())
+//        );
+//        if ("desc".equalsIgnoreCase(type)) {
+//            cmp = cmp.reversed();
+//            dto.getData().getContent().sort(cmp);
+//        }
+//        if ("asc".equalsIgnoreCase(type)) {
+//            dto.getData().getContent().sort(cmp);
+//        }
+//        return dto;
     }
 }
